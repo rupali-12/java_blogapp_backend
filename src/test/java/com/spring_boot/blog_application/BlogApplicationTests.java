f663b48d68a512e908a33afcce93fbf5ad61c063
rupali-12 <rupalisharma1009@gmail.com>
2024-08-29 12:16:32 +0530

    First commit

--- src/test/java/com/spring_boot/blog_application/BlogApplicationTests.java
+++ src/test/java/com/spring_boot/blog_application/BlogApplicationTests.java
@@ -1,24 +1,25 @@
 package com.spring_boot.blog_application;
 
-import com.spring_boot.blog_application.repositories.UserRepo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.spring_boot.blog_application.repo.UserRepository;
 
 @SpringBootTest
 class BlogApplicationTests {
 
 	@Autowired
-	private UserRepo userRepo;
+	private UserRepository userRepo;
 
 	@Test
 	void contextLoads() {
 	}
 
 	@Test
-	public void repoTest(){
+	public void repoTest() {
 		String className = this.userRepo.getClass().getName();
-		String packName=this.userRepo.getClass().getPackageName();
+		String packName = this.userRepo.getClass().getPackageName();
 		System.out.println(className);
 		System.out.println(packName);
 	}
